a6beb42b2ee86683f641d28ac849e34807fd8058
ninjatrack <ninjatrack2020@gmail.com>
2020-11-06 09:27:48 -0600

    store configuration

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,55 +2,11 @@ import * as CONSTANTS from './constants';
 
 // handle users
 const loadAllUsers = () => {
-  return {
-    type: CONSTANTS.LOAD_ALL_USERS,
-  }
-};
-
-const searchUser = (ccNumber) => {
-  return {
-    type: CONSTANTS.SEARCH_USER,
-    payload: ccNumber,
-  }
-};
-
-const readUser = (ccNumber) => {
-  return {
-    type: CONSTANTS.READ_USER,
-    payload: ccNumber,
-  };
-};
-
-// handle products
-const addProduct = (name, cost) => {
-  return {
-    type: CONSTANTS.ADD_PRODUCT,
-    name,
-    cost,
-  };
-};
-
-const editProduct = (id, name, cost) => {
-  return {
-    type: CONSTANTS.EDIT_PRODUCT,
-    id,
-    name,
-    cost,
-  };
-};
-
-const removeProduct = (ccNumber) => {
-  return {
-    type: CONSTANTS.REMOVE_PRODUCT,
-    payload: ccNumber,
-  };
+    return {
+        type: CONSTANTS.LOAD_ALL_USERS,
+    };
 };
 
 export default {
-  loadAllUsers,
-  searchUser,
-  readUser,
-  addProduct,
-  editProduct,
-  removeProduct,
+    loadAllUsers,
 };
